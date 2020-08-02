496e662e4f05cdb6093be95b2d2d25b9daad6c6a
Jorge Salas <js.2701e@gmail.com>
2020-08-01 19:48:34 -0500

    POO examples solved

--- Day_26-SOlid/ejercicios.js
+++ Day_26-SOlid/ejercicios.js
@@ -5,21 +5,11 @@
 	El constructor pide nombre, edad,sexo,peso y altura
     Generar el RFC a partir de el nombre, edad y sexo */
 
-/*  2.  Crear una clase Cuenta que tenga los siguientes atributos y metodos:
-	-Titular y cantidad
-	-ingresar(cantidad)
-	-retirar(cantidad)
-	Hacer las validaciones previas
-	Como regla de negocio no debe de tener más de $900 y menos de $10
- */
-
-
-
 class Persona {
 
     constructor(nombre, apellidoP, apellidoM, edad, diaN, mesN, añoN, rfc, sexo, peso, altura) {
         this.nombre = nombre
-        this.apeelidoPaterno = apellidoP
+        this.apellidoPaterno = apellidoP
         this.apellidoMaterno = apellidoM
         this.diaNacimiento = diaN
         this.mesNacimiento = mesN
@@ -33,20 +23,69 @@ class Persona {
 
     calcularIMC() {
         console.log(this.nombre.length);
-        var IMC = this.peso/((this.altura/100)*(this.altura/100))
+        var IMC = this.peso / ((this.altura / 100) * (this.altura / 100))
         console.log(IMC);
     }
 
-    esMayorDeEdad(){
-        if(this.edad >= 18){
+    esMayorDeEdad() {
+        if (this.edad >= 18) {
             console.log("Puedes pasar");
-        }else{
+        } else {
             console.log("Ve a la choza de los pequeñines");
         }
     }
 
+    calcularRFC() {
+        var str = '';
+        var ref = 'abcdefghijklmnñopqrstuvwxyz';
+        for (var i = 0; i < 2; i++) {
+            str += ref.charAt(Math.floor(Math.random() * ref.length));
+        }
+        var aleatorio = Math.round(Math.random()*10);
+        var vocal;
+        for (var i = 0; i < this.apellidoMaterno.length; i++) {
+            if (this.apellidoPaterno[i] == 'a' || this.apellidoPaterno[i] == 'e' || this.apellidoPaterno[i] == 'i' || this.apellidoPaterno[i] == 'o' || this.apellidoPaterno[i] == 'u') {
+                vocal = this.apellidoPaterno[i]
+                break;
+            }
+        }
+        this.rfc = this.apellidoPaterno[0] + vocal.toUpperCase() + this.apellidoMaterno[0] + this.nombre[0] + this.añoNacimiento[2] + this.añoNacimiento[3] + this.mesNacimiento + this.diaNacimiento + str.toUpperCase() + aleatorio;
+        console.log(this.rfc);
+    }
+
 }
 
-var persona1 = new Persona("Raul", "Arosemena", "Salgado", 25, 30, 03, 1995, "", "M", 80, 180);
+var persona1 = new Persona("Raul", "Arosemena", "Salgado", 25, "30", "03", "1995", "", "M", 80, 180);
 persona1.calcularIMC();
 persona1.esMayorDeEdad();
+persona1.calcularRFC();
+
+
+/*  2.  Crear una clase Cuenta que tenga los siguientes atributos y metodos:
+	-Titular y cantidad
+	-ingresar(cantidad)
+	-retirar(cantidad)
+	Hacer las validaciones previas
+	Como regla de negocio no debe de tener más de $900 y menos de $10
+ */
+
+class Cuenta {
+
+    constructor(titular) {
+        this.titular = titular;
+        this.cantidad = 0;
+    }
+
+    ingresar(deposito) {
+        var enCuenta = this.cantidad += deposito;
+        if(enCuenta < 10 || enCuenta > 900){
+            console.log("El saldo de tu cuenta no puede ser menor que $10.00 ni mayor que $900.00");
+        }else{
+            enCuenta
+            console.log("La cantidad en la cuenta de " + this.titular + " es de " + this.cantidad);
+        }
+    }
+}
+
+var Cuentahabiente = new Cuenta("Carlos Perez");
+Cuentahabiente.ingresar(200);
--- Day_26-SOlid/manchado.js
+++ Day_26-SOlid/manchado.js
@@ -26,3 +26,103 @@ Change the last book read to be the book that just got finished
 Change the current book to be the next book to be read
 Change the next book to be read property to be the first unread book you find in the list of books
 Booklists and Books might need more methods than that. Try to think of more that might be useful. */
+
+
+class BookList {
+
+    constructor() {
+        
+        this.booklist = [
+            {
+                title: 'Harry Potter',
+                genre: 'Fantasy',
+                author: 'J.K. Rowling',
+                read: 'No',
+                readDate: '',
+            },
+            {
+                title: 'Game of Thrones',
+                genre: 'Fantasy',
+                author: 'R.R. Martin',
+                read: 'No',
+                readDate: '',
+            },
+            {
+                title: 'Divina Comedia',
+                genre: 'Poetry',
+                author: 'Dante Alighieri',
+                read: 'No',
+                readDate: '',
+            },
+            {
+                title: 'Don Quijote de la Mancha',
+                genre: 'Novel',
+                author: 'Miguel de Cervantes Saavedra',
+                read: 'No',
+                readDate: '',
+            },
+            {
+                title: 'El amor en los tiempos del cólera',
+                genre: 'Novel',
+                author: 'Gabriel García Márquez',
+                read: 'No',
+                readDate: '',
+            }
+        ];
+        this.readBooks = 0;
+        this.notRead = 5;
+        this.next = this.booklist[1];
+        this.current = this.booklist[0];
+        this.last = "";
+    }
+
+    add(book) {
+        //should add a book to the books list.
+        this.booklist.push(book);
+        console.log(this.booklist);
+    }
+
+    finishCurrentBook() {
+        /* should mark the book that is currently being read as "read"
+        Give it a read date of new Date(Date.now())
+        Change the last book read to be the book that just got finished
+        Change the current book to be the next book to be read
+        Change the next book to be read property to be the first unread book you find in the list of books*/
+        var next;
+        this.current.read = "Yes";
+        this.current.readDate = new Date();
+        this.last = this.current;
+        this.current = this.next;
+        this.current.read = 'Yes';
+        for (var i = 0; i < this.booklist.length; i++) {
+            if (this.booklist[i].read == 'No') {
+                next = this.booklist[i];
+                break;
+            }
+        }
+        this.next = next;
+        //console.log(this.booklist);
+        //console.log(this.current);
+        //console.log(this.next);
+    }
+}
+
+class Book {
+
+    constructor(title, genre, author, read, date) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.read = read;
+        this.readDate = date;
+    }
+
+}
+
+var harry2 = new Book("Harry Potter and the chamber of secrets", "Fantasy", "J.K. Rowling", "Yes", new Date());
+var got2 = new Book("Game of Thrones: Song of Fire and Ice", "Fantasy", "R.R. Martin", "Yes", new Date(2020, 03, 12));
+// const bookList = [harry, got];
+
+var newBooklist = new BookList()
+newBooklist.add(harry2);
+newBooklist.finishCurrentBook();
